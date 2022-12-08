6e0e24922dc35be81002e47b08b474137e0f9417
Micah Russell <108246578+thknkg@users.noreply.github.com>
2022-12-08 15:02:50 -0600

    sheesh

--- advent_of_code/advent_day_7/day_7.py
+++ advent_of_code/advent_day_7/day_7.py
@@ -1,93 +1,45 @@
-import anytree
-from anytree import Node, RenderTree
+"emulate directory navigation and add up directory sizes"
 
-with open('day_7.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
+from itertools import accumulate
+from collections import defaultdict
 
-top = Node("/").parent
-w = anytree.Walker()
-out = ".."
-command = "$"
-change_dir = "cd"
-list_file = "ls"
-directory = "dir"
-current_dir = top
-ln = text.split("\n")
-child_dirs = []
-new_dir = False
-node_list = []
-dir_dict = {}
+# counter
+directory_sizes = defaultdict(int)
 
+# open file
+for line in open(r"C:\Users\micah\PycharmProjects\python-practice-and-challenges\advent_of_code\advent_day_7\day_7.txt"):
+    # split line by command inputs
+    match line.split():
+        # return to root directory
+        case "$", "cd", "/":
+            current_directory = ["/"]
 
-def check_for_new_dir(ln):
-    for i in range(len(ln)):
-        new_dir = False
-        current_line = ln[i]
-        cl = current_line
-        if command in cl and list_file in cl:
-            previous_line = ln[i-1].strip(command + " " + change_dir)
-            for j in range(len(ln) - 1):
-                next_lines = ln[j + 1]
-                if new_dir == False:
-                    if command not in next_lines:
-                        if directory in next_lines:
-                            if next_lines not in dir_dict.values():
-                                dir_dict[previous_line] = []
-                                dir_dict[previous_line].append(next_lines)
+        # do nothing
+        case "$", "ls":
+            pass
 
+        # remove the last directory to move out one
+        case "$", "cd", "..":
+            current_directory.pop()
 
-                        elif command in next_lines:
-                            new_dir = True
+        # move to the given directory by adding to list
+        case "$", "cd", directory:
+            current_directory.append(directory + "/")
 
-                if new_dir == True:
-                    break
+        # do nothing
+        case "dir", directory:
+            pass
 
-def print_dir_files(ln):
-    for i in range(len(ln)):
-        new_dir = False
-        current_line = ln[i]
-        cl = current_line
-        if command in cl:
-            for j in range(len(ln) - 1):
-                next_lines = ln[j + 1]
-                if new_dir == False:
-                    if command not in next_lines:
-                        if directory not in next_lines:
-                            if next_lines not in child_dirs:
-                                child_dirs.append(next_lines)
-                                node_list.append(Node(next_lines))
+        # if it has a size and filename, add size to the size dictionary with given directory as the key
+        case size, file_name:
+            for directory in accumulate(current_directory):
+                directory_sizes[directory] += int(size)
 
+# print total of all that are less than 100_000
+print(sum([value for value in directory_sizes.values() if value <= 100000]))
 
-                        elif command in next_lines:
-                            new_dir = True
+# print smallest directory that, if deleted, would free up enough space
+total_space = 70_000_000
+unused_space = 30_000_000
+print(min(value for value in directory_sizes.values() if value >= (unused_space - (total_space - directory_sizes["/"]))))
 
-                if new_dir == True:
-                    break
-
-for i in range(len(ln)):
-    current_line = ln[i]
-    cl = current_line
-    if command in cl and list_file in cl:
-        dr = ln[i-1].strip("$ cd ")
-        if dr not in dir_dict.keys():
-            dir_dict[dr] = []
-        node_list.append(Node(ln[i-1].strip("$ cd ")))
-
-        for i in range(1, len(ln) - 1):
-            next_line = ln[i + 1]
-            if command not in next_line:
-                child_dirs.append(next_line)
-                dir_dict[dr].append(next_line)
-            elif command in next_line:
-                break
-
-            else:
-                break
-
-
-print(node_list)
-print(dir_dict)
-for key in dir_dict:
-    print(key)
-
-print(RenderTree(dir_dict))
